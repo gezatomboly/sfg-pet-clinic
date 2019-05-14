246bc71fae8aaaaa95b08c290acc92f36a907562
Geza Tomboly <>
2019-05-14 19:16:09 +0200

    Implement Interfaces. Closes #5.

---
+++ pet-clinic-data/src/main/java/gezatomboly/spring/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,16 @@
+package gezatomboly.spring.sfgpetclinic.services;
+
+import gezatomboly.spring.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/gezatomboly/spring/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package gezatomboly.spring.sfgpetclinic.services;
+
+import gezatomboly.spring.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
